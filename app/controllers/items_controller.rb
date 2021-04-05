1a626049c44f450184483689d05e9b9b93667c74
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-05 11:19:49 +0900

    記入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to  items_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -18,5 +26,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def i
   end
 end
