d16fe957590c61233d2cb63718fc2daf0cc21457
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-05 10:36:21 +0900

    最初の方

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to  :user
   has_many    :comments
+  has_one_attached :image
+
+  validates :title,      presence: true
+  validates :concept,    presence: true
+  validates :image,      presence: true
 end
