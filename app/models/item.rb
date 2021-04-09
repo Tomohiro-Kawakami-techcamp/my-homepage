8c2c96fe154367c4a7a6d9b90024170024b6d413
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-04-09 10:20:44 +0900

    検索機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,12 @@ class Item < ApplicationRecord
   validates :title,      presence: true
   validates :concept,    presence: true
   validates :image,      presence: true
+
+  def self.search(search)
+    if search != ""
+      Item.where('title LIKE(?)', "%#{search}%")
+    else
+      Item.all
+    end
+  end
 end
